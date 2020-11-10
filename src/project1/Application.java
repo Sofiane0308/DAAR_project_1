cda01b47f0d14ec96754de1ad44fc8e15ea7fa9e
sofiane0308 <fs_chikhbled@esi.dz>
2020-11-10 11:22:37 +0100

    Regex to automaton

---
+++ src/project1/Application.java
@@ -0,0 +1,124 @@
+package project1;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Application {
+
+	public static void main(String[] args) {
+		Automaton a = buildOperandAutomaton('a');
+		print2D(a.getTransitions());
+		System.out.println("==========");
+		print2D(a.toTable());
+		
+	}
+    static final int EPSILON = 1;
+
+	private static Automaton buildStarAutomaton(Automaton a1) {
+
+		Integer[][] transitions1 = a1.getTransitions();
+		int len1 = transitions1.length;
+		int len = len1 + 2;
+
+		Integer[][] transitions = new Integer[len][len];
+
+		for (int i = 0; i < len1; i++) {
+			for (int k = 0; k < len1; k++) {
+				transitions[i + 1][k + 1] = transitions1[i][k];
+			}
+		}
+
+		transitions[0][len - 1] = EPSILON;
+		transitions[0][1] = EPSILON;
+		transitions[len1][1] = EPSILON;
+		transitions[len1][len - 1] = EPSILON;
+
+		Automaton a = new Automaton(transitions, 0, len - 1);
+		return a;
+	}
+
+	private static Automaton buildConcatAutomaton(Automaton a1, Automaton a2) {
+
+		Integer[][] transitions1 = a1.getTransitions();
+		Integer[][] transitions2 = a2.getTransitions();
+		int len1 = transitions1.length;
+		int len2 = transitions2.length;
+		int len = len1 + len2;
+
+		Integer[][] transitions = new Integer[len][len];
+
+		for (int i = 0; i < len1; i++) {
+			for (int k = 0; k < len1; k++) {
+				transitions[i][k] = transitions1[i][k];
+			}
+		}
+		for (int i = 0; i < len2; i++) {
+			for (int k = 0; k < len2; k++) {
+				transitions[i + len1][k + len1] = transitions2[i][k];
+			}
+		}
+
+		transitions[len1 - 1][len1] = EPSILON;
+		Automaton a = new Automaton(transitions, 0, len - 1);
+		return a;
+	}
+
+	private static Automaton buildAlternateAutomaton(Automaton a1, Automaton a2) {
+
+		Integer[][] transitions1 = a1.getTransitions();
+		Integer[][] transitions2 = a2.getTransitions();
+		int len1 = transitions1.length;
+		int len2 = transitions2.length;
+		int len = len1 + len2;
+
+		Integer[][] transitions = new Integer[len + 2][len + 2];
+
+		transitions[0][1] = EPSILON;
+		transitions[0][len1 + 1] = EPSILON;
+
+		for (int i = 0; i < len1; i++) {
+			for (int k = 0; k < len1; k++) {
+				transitions[i + 1][k + 1] = transitions1[i][k];
+			}
+		}
+		for (int i = 0; i < len2; i++) {
+			for (int k = 0; k < len2; k++) {
+				transitions[i + 1 + len1][k + 1 + len1] = transitions2[i][k];
+			}
+		}
+
+		transitions[len1][len + 1] = EPSILON;
+		transitions[len][len + 1] = EPSILON;
+		Automaton a = new Automaton(transitions, 0, len + 1);
+		return a;
+	}
+
+	private static Automaton buildOperandAutomaton(int c) {
+		Integer[][] transitions = { { null, c }, { null, null } };
+		Automaton automaton = new Automaton(transitions, 0, 1);
+		return automaton;
+	}
+
+	public static void print2D(Integer mat[][]) {
+		// Loop through all rows
+		for (int i = 0; i < mat.length; i++) {
+			// Loop through all elements of current row
+			for (int j = 0; j < mat[i].length; j++)
+				System.out.print(mat[i][j] + " ");
+			System.out.println("");
+		}
+
+	}
+	public static void print2D(ArrayList<Integer>[][] mat) {
+		// Loop through all rows
+		for (int i = 0; i < mat.length; i++) {
+			// Loop through all elements of current row
+			for (int j = 0; j < mat[i].length; j++)
+				System.out.print(mat[i][j] + " ");
+			System.out.println("");
+		}
+
+	}
+
+}
---
+++ src/project1/Automaton.java
@@ -0,0 +1,104 @@
+package project1;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Automaton {
+    private int start;
+    private int end;
+    private int current;
+    private Integer[][] transitions;
+    
+    
+    static final int EPSILON = 1;
+
+    public Automaton(Integer[][] transitions, int start, int end) {
+        this.start = start;
+        this.end = end;
+        this.current = start;
+        this.transitions = transitions; 
+    }
+    //Create a cell for the table
+    ArrayList<Integer> createCell(Integer i ){
+    	ArrayList<Integer> list = new ArrayList<Integer>();
+    	list.add(i);
+    	return list;
+    }
+    //Convert automaton to table
+    ArrayList<Integer>[][] toTable(){
+    	//collect character list
+    	Set<Integer> set =  new HashSet<Integer>() ;
+    	for(int i =0; i< transitions.length; i++) {
+    		for (int j = 0; j<transitions[0].length; j++) {
+    			Integer cell = transitions[i][j];
+    			if(cell != null && cell != EPSILON) set.add(cell);
+    		}
+    	}
+    	int nbCols = set.size() + 2;
+    	int nbRows = transitions.length + 1; 
+    	
+    	ArrayList<Integer>[][] table = new ArrayList[nbRows][nbCols];
+    	//build character row
+    	table[0][1] = createCell(EPSILON);
+    	Iterator<Integer> it = set.iterator();
+    	int k = 2;
+    	while(it.hasNext()) {
+    		table[0][k] = createCell(it.next());
+    		k++;
+    	}
+    	//fill the table
+    	for(int i=0; i<transitions.length;i++) {
+    		table[i+1][0] = createCell(i);
+    		for(int j = 0; j<transitions[0].length; j++) {
+    			Integer c = transitions[i][j];
+    			if(c != null) {
+    				//get char col number
+        			k = 1;
+        			boolean found = false;
+        			while(!found && k < table[0].length) {
+        				if (table[0][k].get(0)== c) found = true;
+        				k++;
+        			}
+        			k--;
+        			table[i+1][k] = createCell(j);
+    			}
+    			
+    		}
+    	}
+    	return table;
+    }
+    
+    public Integer[][] getTransitions() {
+        return transitions;
+    }
+
+    public void setTransitions(Integer[][] transitions) {
+        this.transitions = transitions;
+    }
+
+    public int getCurrent() {
+        return current;
+    }
+
+    public void setCurrent(int current) {
+        this.current = current;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+}
